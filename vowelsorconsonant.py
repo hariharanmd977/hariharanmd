6e10db76cae75f99fe3148e845faa634a1556e1c
hariharanmd977 <52821641+hariharanmd977@users.noreply.github.com>
2019-07-31 19:29:00 +0530

    Create vowelsorconsonant.py

---
+++ vowelsorconsonant.py
@@ -0,0 +1,9 @@
+n=input()
+if((n>='a' and n<='z') or (n>='A' and n<='Z')):
+  if(n== 'a' or n == 'e' or n == 'i' or n == 'o' or n == 'u' or n == 'A'
+       or n == 'E' or n == 'I' or n == 'O' or n == 'U'):
+       print("Vowel")
+  else:
+       print("Consonant")
+else:
+  print("invalid")
